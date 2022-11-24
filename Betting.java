4f319bff61b42276a13a8ae26b172b0898d4174d
pzid01 <108907320+pzid01@users.noreply.github.com>
2022-11-24 12:32:59 -0500

    Initial commit of betting class with dummy code

---
+++ Betting.java
@@ -0,0 +1,40 @@
+package assignment4;
+
+public class Betting  {
+	
+	public Betting(int minBalance, IRandomValueGenerator randomGenerator) {
+	}
+
+	public int getCurrentBalance() {
+		return 0;
+	}
+
+	public boolean canBet(double amnt) {
+		return false;
+	}
+
+	public void addMoney(double amnt) {		
+		
+	}
+
+	public double betOnANumber(double amnt, int min, int max, int selectedNumber) {
+		return 0;
+	}
+
+	public double betOnProbability(double amnt, double p) {
+		return 0;
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
