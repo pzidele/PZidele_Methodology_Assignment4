e47f521753745b551870ef85eaf4b476bb95b21c
pzidele <108907320+pzidele@users.noreply.github.com>
2022-12-25 01:03:49 -0500

    Removed comments

--- IRandomValueGenerator.java
+++ IRandomValueGenerator.java
@@ -2,10 +2,7 @@ package assignment4;
 
 public interface IRandomValueGenerator {
 	
-	
-	//public int getRandomRange(int min, int max);
-	
-	//public double getRandomProb();
+
 	
 	public int generateRandom(int min, int max);
 	
